83b114804926e36492d2a097967877fb8cb6ec1e
Your Name <victor_bankowski@hotmail.com>
2019-02-11 23:39:19 +0200

    t9 tehty

---
+++ build/precache-manifest.5e53ae75b21eb6ff53c36a6942f66078.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e563a1923bc1af79f80",
+    "url": "/static/js/main.7e563a19.chunk.js"
+  },
+  {
+    "revision": "4bd0552fbcbbfdf0aa8f",
+    "url": "/static/js/1.4bd0552f.chunk.js"
+  },
+  {
+    "revision": "8219234727fd28f583412739416da362",
+    "url": "/index.html"
+  }
+];
